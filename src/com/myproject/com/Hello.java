64835fdb1c9cd142e4edacb43b6c0b8f302f8315
ganesanmurugan25 <ganesanmurugan25@gmail.com>
2020-06-17 10:21:24 +0530

    initial commit

---
+++ src/com/myproject/com/Hello.java
@@ -0,0 +1,11 @@
+package com.myproject.com;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World! I am well!");
+		// TODO Auto-generated method stub
+
+	}
+
+}
